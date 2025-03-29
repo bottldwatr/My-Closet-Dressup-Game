d4ff2dcf0eb70c65a936ba1e79675e33130a1f6d
Joe Czerniecki <s4072773@student.rmit.edu.au>
2025-03-30 07:02:42 +1100

    Made main box responsively sized and begun implementation on categories carousel

---
+++ dressup/src/app/components/carousel.tsx
@@ -0,0 +1,42 @@
+export default function Carousel() {
+    return (
+        <div className="p-5 box-border flex flex-row gap-5 bg-green-300">
+            <div className="lg:w-18 lg:h-18 bg-red-500 flex items-center justify-center">
+                <p>Shirt</p>
+            </div>
+            <div className="lg:w-18 lg:h-18 bg-red-500 flex items-center justify-center">
+                <p>Overshirt</p>
+            </div>
+            <div className="lg:w-18 lg:h-18 bg-red-500 flex items-center justify-center">
+                <p>Jacket/Jumper</p>
+            </div>
+            <div className="lg:w-18 lg:h-18 bg-red-500 flex items-center justify-center">
+                <p>Pants</p>
+            </div>
+            <div className="lg:w-18 lg:h-18 bg-red-500 flex items-center justify-center">
+                <p>Skirt</p>
+            </div>
+            <div className="lg:w-18 lg:h-18 bg-red-500 flex items-center justify-center">
+                <p>Shoes</p>
+            </div>
+            <div className="lg:w-18 lg:h-18 bg-red-500 flex items-center justify-center">
+                <p>Socks</p>
+            </div>
+            <div className="lg:w-18 lg:h-18 bg-red-500 flex items-center justify-center">
+                <p>Accessory 1</p>
+            </div>
+            <div className="lg:w-18 lg:h-18 bg-red-500 flex items-center justify-center">
+                <p>Accessory 2</p>
+            </div>
+            <div className="lg:w-18 lg:h-18 bg-red-500 flex items-center justify-center">
+                <p>Accessory 3</p>
+            </div>
+            <div className="lg:w-18 lg:h-18 bg-red-500 flex items-center justify-center">
+                <p>Accessory 4</p>
+            </div>
+            <div className="lg:w-18 lg:h-18 bg-red-500 flex items-center justify-center">
+                <p>Accessory 5</p>
+            </div>
+        </div>
+    )
+}
--- dressup/src/app/page.tsx
+++ dressup/src/app/page.tsx
@@ -1,9 +1,11 @@
+import Carousel from "./components/carousel";
+
 export default function Home() {
   return (
     <div className="w-screen h-screen bg-[url(../../public/room-bg.gif)] bg-cover flex flex-row justify-center items-center">
-      <div className="w-100 h-100 bg-white">
-        <h1 className="text-red-500 text-9xl italic">Hello</h1>
-        <h1 className="text-red-500 text-9xl">Pooch</h1>
+      <div className="w-70 sm:w-80 md:w-167 lg:w-200 h-120 sm:h-144 md:h-90 lg:h-120 bg-white flex flex-col items-center">
+        <h1 className="text-black text-2xl text-2xl md:text-3xl lg:text-4xl">Please Select a category</h1>
+        <Carousel />
       </div>
     </div>
   );
