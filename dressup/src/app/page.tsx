758ec967cce6b17ed82be43c7bfde8013430fba5
Joe Czerniecki <s4072773@student.rmit.edu.au>
2025-03-29 16:40:23 +1100

    Set up NextJS Set up project for future development

---
+++ dressup/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="w-screen h-screen bg-[url(../../public/room-bg.gif)] bg-cover flex flex-row justify-center items-center">
+      <div className="w-100 h-100 bg-white">
+        <p className="text-red-500">Hello</p>
+      </div>
+    </div>
+  );
+}
